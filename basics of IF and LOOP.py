9ea917c4489159c9e5a9f3efef747432e435f139
uddeshya kumar <uddeshya2012@gmail.com>
2021-10-26 06:01:48 +0530

    Initial commit

---
+++ basics of IF and LOOP.py
@@ -0,0 +1,15 @@
+print("hello world.")
+# loop
+for x in [1,4,5,6]:
+    print(x)
+# loop type-2
+for y in range(7):
+ print(y)
+
+ # if statements
+ #if (condition):
+  #   do this
+ #elif (condition2):
+  #   do that
+ #else:
+  #   then do this
